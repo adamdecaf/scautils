72a6dd6947d6ab9ed00bca3108d7f20ef49bc9d4
Adam Shannon <adam@ashannon.us>
2012-08-22 21:30:52 -0500

    Comment out some code that doesn't work for now.

--- src/main/scala/ForAll.scala
+++ src/main/scala/ForAll.scala
@@ -1,20 +1,20 @@
 package com.scautils
 
-object ForAll extends ForAll
+// object ForAll extends ForAll
 
-trait ForAll {
+// trait ForAll {
 
-  private[this] var status = false
+//   private[this] var status = false
 
-  def apply[T, A <: { def forAll: T => Boolean }](items: A, cond: T) =
-    items.forAll(cond)
+//   def apply[T, A <: { def forAll: T => Boolean }](items: A, cond: T) =
+//     items.forAll(cond)
 
-  // Call .forAll on a collection which has it implemented.
-  def apply[A](items: Iterable[_], f: Function[A, Boolean]) =
-    apply(items.toIterator, f)
+//   // Call .forAll on a collection which has it implemented.
+//   def apply[A](items: Iterable[_], f: Function[A, Boolean]) =
+//     apply(items.toIterator, f)
 
-  // Normal version, where the cond is user-supplied
-  def apply[A](items: Iterator[A], f: Function1[A, Boolean]) =
-    AlwaysAct(items.next, f)((bool: Boolean) => status = true, (bool: Boolean) => status = false)
+//   // Normal version, where the cond is user-supplied
+//   def apply[A](items: Iterator[A], f: Function1[A, Boolean]) =
+//     AlwaysAct(items.next, f)((bool: Boolean) => status = true, (bool: Boolean) => status = false)
 
-}
+// }
