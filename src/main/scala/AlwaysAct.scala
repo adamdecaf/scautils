d7706e7a343df2976d081411171a20d62f3b78b4
Adam Shannon <adam@ashannon.us>
2012-08-26 16:19:49 -0500

    Clean up AlwaysAct.

--- src/main/scala/AlwaysAct.scala
+++ src/main/scala/AlwaysAct.scala
@@ -1,17 +1,10 @@
 package com.scautils
 
-// object AlwaysAct extends AlwaysAct
+object AlwaysAct {
 
-// trait AlwaysAct {
+ def apply[A](value: A, cond: A => Boolean)(onTrue: A => Unit, onFalse: A => Unit): Unit = {
+    if (cond(value)) onTrue(value)
+    else onFalse(value)
+  }
 
-//   def apply[A <: Any, B](value: A, cond: A => Boolean)(onTrue: A => B, onFalse: A => B): Unit = {
-//     if (cond(value)) onTrue(value)
-//     else onFalse(value)
-//   }
-
-//   def apply[A](value: A, cond: A => Boolean)(onTrue: A => Unit, onFalse: A => Unit): Unit = {
-//     if (cond(value)) onTrue(value)
-//     else onFalse(value)
-//   }
-
-// }
+}
