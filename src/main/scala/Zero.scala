4192419065d4c339dd4a8944edb03489372ac22c
Adam Shannon <adam@ashannon.us>
2012-08-28 08:43:11 -0500

    Update Zero.

--- src/main/scala/Zero.scala
+++ src/main/scala/Zero.scala
@@ -1,44 +1,39 @@
 package com.scautils
 
+import scala.collection.immutable.{Queue, Stack}
+import scala.reflect.ClassTag
+
 trait Zero[Z] {
   val zero: Z
+
+  def map[B](implicit zero: Zero[B]) = Zero.mzero[B]
+  def flatMap[B](implicit zero: Zero[B]) = Zero.mzero[B]
 }
 
-object Zero {
-  import scala.collection.immutable.{Queue, Stack}
+trait Zeros {
+  implicit def UnitZero = new Zero[Unit] { val zero = () }
+  implicit def BigIntZero = new Zero[BigInt] { val zero = BigInt(0) }
+  implicit def ByteZero = new Zero[Byte] { val zero = 0.toByte }
+  implicit def BooleanZero = new Zero[Boolean] { val zero = false }
+  implicit def CharZero = new Zero[Char] { val zero = 0.toChar }
+  implicit def DoubleZero = new Zero[Double] { val zero = 0D }
+  implicit def FloatZero = new Zero[Float] { val zero = 0F }
+  implicit def IntZero = new Zero[Int] { val zero = 0 }
+  implicit def LongZero = new Zero[Long] { val zero = 0L }
+  implicit def ShortZero = new Zero[Short] { val zero = 0.toShort }
+  implicit def StringZero = new Zero[String] { val zero = "" }
 
-  def apply[Z](item: Z)(implicit m: Manifest[Z]) = new Zero[Z] { val zero: Z = flatten1[Z](item) }
-  def apply[A, Z[A]](item: Z[A])(implicit ma: Manifest[A], maz: Manifest[Z[A]]) = new Zero[Z[A]] { val zero: Z[A] = flatten2[A, Z](item) }
-  // def apply[A, B, T[A, B]](item: T[A, B])(implicit ma: Manifest[A], mb: Manifest[B], mab: Manifest[T[A, B]]) = new Zero[T[A, B]] {
-  //   val zero: T[A, B] = flatten3[A, B, T](item)
-  // }
+  implicit def StackZero[A] = new Zero[Stack[A]] { val zero: Stack[A] = Stack[A]() }
+  implicit def StreamZero[A] = new Zero[Stream[A]] { val zero: Stream[A] = Stream[A]() }
+  implicit def VectorZero[A] = new Zero[Vector[A]] { val zero: Vector[A] = Vector[A]() }
+  implicit def ArrayZero[A: ClassTag] = new Zero[Array[A]] { val zero: Array[A] = Array[A]() }
+  implicit def ListZero[A] = new Zero[List[A]] { val zero: List[A] = List[A]() }
+  implicit def OptionZero[A] = new Zero[Option[A]] { val zero: Option[A] = Option.empty[A] }
+  implicit def QueueZero[A] = new Zero[Queue[A]] { val zero: Queue[A] = Queue[A]() }
+  implicit def SeqZero[A] = new Zero[Seq[A]] { val zero: Seq[A] = Seq[A]() }
+  implicit def SetZero[A] = new Zero[Set[A]] { val zero: Set[A] = Set[A]() }
+}
 
-  private def flatten1[T](item: T)(implicit m: Manifest[T]): T = (item match {
-    case x: Byte => val a: Byte = x; a
-    case x: Boolean => true
-    case x: Char => val a: Char = x; a
-    case x: Double => 1D
-    case x: Float => 1F
-    case x: Int => 1
-    case x: Long => 1L
-    case x: String => ""
-    case x: Unit => ()
-  }).asInstanceOf[T]
-
-  private def flatten2[A, T[A]](item: T[A])(implicit mA: Manifest[A], mTA: Manifest[T[A]]) = (item match {
-    case x: Stack[A] => Stack[A]()
-    case x: Stream[A] => Stream[A]()
-    case x: Vector[A] => Vector[A]()
-    case x: Array[A] => Array[A]()
-    case x: List[A] => List[A]()
-    case x: Option[A] => Option.empty[A]
-    case x: Queue[A] => Queue[A]()
-    case x: Seq[A] => Seq[A]()
-    case x: Set[A] => Set[A]()
-  }).asInstanceOf[T[A]]
-
-  private def flatten3[A, B, T[A, B]](item: T[A, B])(implicit m: Manifest[T[A, B]]) = (item match {
-    case x: Map[A, B] => Map[A, B]()
-  }).asInstanceOf[T[A, B]]
-
+object Zero extends Zeros {
+  def mzero[A](implicit zero: Zero[A]) = zero
 }
